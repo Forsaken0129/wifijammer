44329be06941f48ef39155454131fa6f77cefc6c
Eshan Sharma <37268525+Forsaken0129@users.noreply.github.com>
2022-11-18 02:02:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,7 +52,7 @@ echo -e "-=[ ${Y}${BOLD}INSTALL DEPENDENCIES${RST} ]=-\n"
 apt install xterm -y
 apt install aircrack-ng -y
 apt install mdk4 -y
-mkdir tmp
+mkdir interface
 clear
 echo -e """
 ${R}
